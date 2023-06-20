f094855e126f2a2d13672bd7e4548bf08a68a153
Thijs Kruithof <39615244+thijskruithof@users.noreply.github.com>
2023-06-20 13:18:07 +0200

    Added a proper license and copyright text to Pyxis

--- WaveSabreCore/include/WaveSabreCore/Pyxis.h
+++ WaveSabreCore/include/WaveSabreCore/Pyxis.h
@@ -1,3 +1,37 @@
+/**
+Pyxis, an elaborate reverb.
+
+Based on the Clouds texture synthesizer,
+https://pichenettes.github.io/mutable-instruments-documentation/modules/clouds/
+
+Copyright 2014 Emilie Gillet.
+Author: Emilie Gillet (emilie.o.gillet@gmail.com)
+
+Copyright for portions of project WaveSabre (forked by Inque) are held by WabeSabre Team 2012-2019 as part of project WaveSabre.
+
+All other copyright for project WaveSabre (forked by Inque) are held by Inque, 2020.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+See http://creativecommons.org/licenses/MIT/ for more information.
+**/
+
 #ifndef __WAVESABRECORE_PYXIS_H__
 #define __WAVESABRECORE_PYXIS_H__
 
@@ -7,11 +41,6 @@
 
 namespace WaveSabreCore
 {
-	/**
-	Pyxis, an elaborate reverb.
-	Based on the Clouds texture synthesizer,
-	https://pichenettes.github.io/mutable-instruments-documentation/modules/clouds/
-	**/
 	class Pyxis : public Device
 	{
 	public:
